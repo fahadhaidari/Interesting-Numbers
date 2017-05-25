398eab0a2f75c09f9d26b2220017baacb025e5fb
fahadhaidari <fahad.haidari@gmail.com>
2017-05-25 14:24:30 -0600

    change the preloader color and remove the console and changed it to logger

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 const CORS_URL = "https://cors-anywhere.herokuapp.com/";
 var URL = "http://numbersapi.com/"; // the Numbers-API URL
-var consoleLogger = undefined;
 var searchField = undefined;
-var preloader = undefined;
+var logger = undefined;
 var numberRegularExpression = undefined;
 var numberToCheckFor = 0;
 
 window.onload = function() {
   console.log("Window is loaded");
-  consoleLogger = Utility.getElement("console"); // it's the container that shows the result
-  searchField   = Utility.getElement("search"); // search-field, where user enter the number he/she is interested in
-  preloader     = Utility.getElement("preloader"); // display this preloader while fetching the result
+  // consoleLogger = Utility.getElement("console"); // it's the container that shows the result
+  searchField = Utility.getElement("search"); // search-field, where user enter the number he/she is interested in
+  logger      = Utility.getElement("logger"); // display this preloader while fetching the result
 
   // listeners
   search.addEventListener("keyup", onKeyUp);
@@ -27,20 +26,21 @@ function onKeyUp(event) {
     numberToCheckFor = searchField.value; // get the value entered in the search-field
 
     if (numberRegularExpression.test(numberToCheckFor)) { // check if it's a number
-      preloader.style.display = "block"; // display preloader
+      // preloader.style.display = "block"; // display preloader
+      logger.innerHTML = "Looking for number " + numberToCheckFor + " for you";
       HTTPRequest.get(CORS_URL + URL + numberToCheckFor).then(callback); // hit the URL
     } else {
-      consoleLogger.innerHTML = "Please enter a number"; // user did not enter a number
+      // consoleLogger.innerHTML = "Please enter a number"; // user did not enter a number
     }
 
   }
 }
 
 function onClick(event) {
-  Utility.getElement(searchField).focus(); // always focus on the search-field
+  searchField.focus(); // always focus on the search-field
 }
 
 function callback(result) {
-  consoleLogger.innerHTML = result; // display the results
-  preloader.style.display = "none"; // hide the preloader
+  logger.innerHTML = result; // display the results
+  //preloader.style.display = "none"; // hide the preloader
 }
